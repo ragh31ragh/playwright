8b98a6708277dd862a91c37bedb07e8231a26303
raghavendra d <ghanahamsa14@gmail.com>
2025-04-01 18:03:37 +0530

    code changes

---
+++ playwright/test_playwrightBasics.py
@@ -0,0 +1,22 @@
+import time
+
+from playwright.sync_api import Page
+
+def test_playwrightBasics(playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://rahulshettyacademy.com")
+
+def test_playwrightShortCut(page:Page):
+    page.goto("https://rahulshettyacademy.com")
+
+def test_coreLocators(page:Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise")
+    page.get_by_label("Username:").fill("rahulshettyacademy")
+    page.get_by_label("Password:").fill("learning")
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
+    page.get_by_role("link",name="terms and conditions")
+    page.get_by_role("button",name="Sign In").click()
+    time.sleep(5)
