193125943de502e36a9729881cf6c0affda31d4b
raghavendra d <ghanahamsa14@gmail.com>
2025-04-16 10:56:39 +0530

    additional changes

--- playwright/conftest.py
+++ playwright/conftest.py
@@ -1,6 +1,31 @@
 import pytest
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name",
+        action="store",
+        default="chrome",
+        help="Browser selection"
+    )
+    ##we can add one more code for url also here
+
+
+
+
 
 @pytest.fixture(scope="session")
 def user_creds(request):
     return request.param
+
+@pytest.fixture
+def browserInstance(playwright,request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        browser = playwright.chromium.launch(headless=False)
+    elif browser_name == "firefox":
+        browser = playwright.firefox.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    yield page
+    context.close()
+    browser.close()
--- playwright/interview_prep/practice_only.py
+++ playwright/interview_prep/practice_only.py
@@ -1,9 +1,6 @@
-numbers1 = [ 31,12,23,42,5,36,8]
-tripled_numbers = map(lambda x : x*3 ,numbers1)
-print(list(tripled_numbers))
+for x in range(5,0,-1):
+    for y in range(x):
+        print("* ",end="")
+    print("\t")
+    #print("*")
 
-even_numbers = filter(lambda x : x%2 == 0 , numbers1)
-print(list(even_numbers))
-
-sorted_List = sorted(numbers1)
-print(sorted_List)
